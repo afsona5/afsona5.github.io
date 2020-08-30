8e14babdc9ee5cfe065d9d8fe54932493988a0af
afsona5 <67174754+afsona5@users.noreply.github.com>
2020-08-30 15:28:28 +0200

    Add files via upload

---
+++ precache-manifest.d9bc7c32976e3b4235b15688bceee799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2697f144bfc1c0bc57e024a1c46fddbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fad5998f31358bd60a3",
+    "url": "/static/css/main.192c7445.chunk.css"
+  },
+  {
+    "revision": "d71ac4888b68f14107f1",
+    "url": "/static/js/2.e7395faa.chunk.js"
+  },
+  {
+    "revision": "5fad5998f31358bd60a3",
+    "url": "/static/js/main.b947b3e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
